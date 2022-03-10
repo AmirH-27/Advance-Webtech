dbbbbea5f1b3b2667a9445fbeed2751c52730feb
AmirH-27 <habibamir92@gmail.com>
2022-03-10 12:19:14 +0600

    Mid Task 3
    Database, Models, Session, Middleware, CRUD

---
+++ APWT_TASK_3/resources/views/inc/topNavAdmin.blade.php
@@ -0,0 +1,4 @@
+<a class = "btn btn-primary" href="{{route('homeAdmin')}}">Home</a>
+<a class = "btn btn-primary" href="{{route('profileAdmin')}}">Profile</a>
+<a class = "btn btn-primary" href="{{route('listAdmin')}}">User List</a>
+<a class = "btn btn-secondary" href="{{route('logout')}}">Logout</a>
---
+++ APWT_TASK_3/routes/web.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\RegistrationController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('Pages.Login');
+});
+//----------------------------Admin----------------------------//
+Route::get('/homeAdmin', [AdminController::class, 'index'])->name('homeAdmin');
+Route::get('/profileAdmin', [AdminController::class, 'profile'])->name('profileAdmin')->middleware('ValidAdmin');
+Route::post('/profileAdmin', [AdminController::class, 'profileEdit'])->name('profileAdminEdit');
+Route::get('/admin/list', [AdminController::class, 'list'])->name('listAdmin');
+Route::get('/logout', [AdminController::class, 'logout'])->name('logout');
+Route::get('/editUser/{id}', [AdminController::class, 'editUser']);
+Route::post('/editUser', [AdminController::class, 'editUserSubmit'])->name('editUser');
+Route::get('/deleteUser/{id}', [AdminController::class, 'deleteUser']);
+Route::post('/deleteUser', [AdminController::class, 'deleteUserSubmit'])->name('deleteUser');
+
+//----------------------------User----------------------------//
+Route::get('/home', [UserController::class, 'index'])->name('homeUser');
+Route::get('/team', [UserController::class, 'team'])->name('teamUser')->middleware('ValidUser');
+Route::get('/profile', [UserController::class, 'profile'])->name('profileUser')->middleware('ValidUser');
+Route::post('/profileAdmin', [UserController::class, 'profileEdit'])->name('profileUserEdit');
+Route::get('/logout', [UserController::class, 'logout'])->name('logout');
+
+
+//----------------------------Login&Registration----------------------------//
+Route::get('/login', [LoginController::class, 'login']);
+Route::post('/login', [LoginController::class, 'loginSubmit'])->name('login');
+
+Route::get('/registration', [RegistrationController::class, 'registration'])->name('registration');
+Route::post('/registration', [RegistrationController::class, 'registrationSubmit'])->name('registration');
